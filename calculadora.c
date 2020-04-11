f46637b360174d77023685c3d50c5c91b104a5e3
Leandro Arias <lea.arias7@gmail.comgit config --global user.name Leandro>
2020-04-11 14:01:16 -0300

    empezando el proyecto

---
+++ calculadora.c
@@ -0,0 +1,28 @@
+# include <stdlib.h>
+#include  <stdio.h>
+#include "calculos.h"
+
+int main() {
+	int numero1, numero2;
+	char respuesta;
+
+	do{
+
+		printf("Bienvenido a  MiCalculadora" );
+		printf("\nIngrese el primer operando: ");
+		scanf("%d", &numero1);
+
+		printf("\nIngrese el segundo operando: ");
+		scanf("%d", &numero2);
+
+
+
+	 printf("\nQuiere realizar la misma operación con números distintos?: (s/n)");
+	 scanf("%c",&respuesta);
+	}while(respuesta == 's');
+
+
+
+	return 0;
+}
+
---
+++ calculos.c
@@ -0,0 +1,11 @@
+
+
+
+int suma(int primero, int segundo) {
+    
+    int resultadoSuma;
+    
+    resultadoSuma = primero + segundo;
+    
+    return resultadoSuma;
+}
